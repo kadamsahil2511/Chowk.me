ee8affee17818be6a3eff4b0bbf5c1b2cd4a93e3
yadavsourabhgh <2024.sourabhy@isu.ac.in>
2025-04-26 11:01:40 +0530

    bug fix 1

--- src/components/PostDetailsModal.js
+++ src/components/PostDetailsModal.js
@@ -99,7 +99,7 @@ const PostDetailsModal = ({ isOpen, onClose, post }) => {
               {post.username && (
                 <div className="flex items-center">
                   <User size={16} className="mr-1" />
-                <span><a href={`http://localhost:3000/user/${post.username}`}>{post.username}</a></span>
+                <span><a href={`/user/${post.username}`}>{post.username}</a></span>
                 </div>
               )}
               
